ef4a00a6bc47223f2d846bd50c82695783d555ec
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-07-22 14:00:17 +0530

    Implement login form stub with datastar

--- apps/server/src/auth.rs
+++ apps/server/src/auth.rs
@@ -15,6 +15,7 @@ pub fn routes() -> Router<AppState> {
         .route("/handle", post(handle_login))
 }
 
+// Templates, Inputs and other structs
 #[derive(Template)]
 #[template(path = "auth/login.html")]
 struct LoginTemplate;
@@ -25,11 +26,24 @@ struct LoginInput {
     password: String,
 }
 
+// Functions
+fn verify_password(password_text: &str) -> bool {
+    if !password_text.is_empty() {
+        return true;
+    }
+    tracing::info!("No password given");
+    false
+}
+
+// Handlers
 async fn login_page() -> Result<impl IntoResponse, AppError> {
     Ok(HtmlTemplate::new(LoginTemplate {}))
 }
 
 async fn handle_login(Form(input): Form<LoginInput>) -> Result<impl IntoResponse, AppError> {
     tracing::info!("Inputs email={}, password={}", input.email, input.password);
+    if verify_password(&input.password) {
+        tracing::info!("Password verified");
+    }
     Ok(Redirect::to("/"))
 }
